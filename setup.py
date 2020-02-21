9adfa38cf4d953e081602f9b0085bcca92de0511
Akshay Maldhure <akshay.m@circles.asia>
2020-02-21 18:51:00 +0800

    Bump up the version to 1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='pybbucket',
     packages=['pybbucket'],
-    version='1.0',
+    version='1.0.1',
     license='MIT',
     description='A Python wrapper for the Bitbucket Cloud REST API 2.0 version.',
     long_description_content_type='text/markdown',
@@ -16,7 +16,7 @@ setup(
     author='Akshay Maldhure',
     author_email='akshaymaldhure@gmail.com',
     url='https://github.com/akshayamaldhure',
-    download_url='https://github.com/akshayamaldhure/pybbucket/archive/v1.0.tar.gz',
+    download_url='https://github.com/akshayamaldhure/pybbucket/archive/v1.0.1.tar.gz',
     keywords=['bitbucket', 'bitbucketapi', 'pybitbucket'],
     install_requires=[
         'pybbucket',
